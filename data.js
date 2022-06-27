c5b0df8bad3a547f8c8ad4404ddb76ddb76b186d
Lander <u2171118@live.warwick.ac.uk>
2022-06-27 16:42:21 +0100

    MSB_002
    002

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-courtyard",
-      "name": "Courtyard",
+      "id": "0-the-courtyard",
+      "name": "The Courtyard",
       "levels": [
         {
           "tileSize": 256,
@@ -24,23 +24,613 @@ var APP_DATA = {
       ],
       "faceSize": 1680,
       "initialViewParameters": {
-        "yaw": 2.7159844888114435,
-        "pitch": 0.04521632637514372,
+        "yaw": 2.35555980037714,
+        "pitch": 0.012678482954363801,
         "fov": 1.35820747241737
       },
       "linkHotspots": [
         {
-          "yaw": -0.2761438116749062,
-          "pitch": 0.021803901468258502,
+          "yaw": 2.492317792093978,
+          "pitch": -0.027016124019366927,
           "rotation": 0,
-          "target": "1-uni_road"
+          "target": "12-zeeman-building---warwick-mathematics-institute"
+        },
+        {
+          "yaw": -0.5590964649481212,
+          "pitch": -0.012517898116167103,
+          "rotation": 0,
+          "target": "1-university-road"
+        },
+        {
+          "yaw": 1.3381472634341183,
+          "pitch": -0.025046238683151145,
+          "rotation": 0,
+          "target": "2-mathematical-sciences-building---front-door"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-uni_road",
-      "name": "Uni_road",
+      "id": "1-university-road",
+      "name": "University Road",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.7059816226361662,
+        "pitch": -0.04730617070036125,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19507266288617053,
+          "pitch": 0.0038790305178846296,
+          "rotation": 0,
+          "target": "0-the-courtyard"
+        },
+        {
+          "yaw": 3.0372445097184873,
+          "pitch": 0.06541706560376248,
+          "rotation": 0,
+          "target": "15-campus-centre---the-piazza"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-mathematical-sciences-building---front-door",
+      "name": "Mathematical Sciences Building - Front door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.4552761274556687,
+        "pitch": -0.24899408441016035,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.918075044980334,
+          "pitch": 0.012246745601924403,
+          "rotation": 0,
+          "target": "3-msb---atrium---entrance"
+        },
+        {
+          "yaw": -1.801596206564085,
+          "pitch": 0.045619928861636794,
+          "rotation": 0,
+          "target": "0-the-courtyard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-msb---atrium---entrance",
+      "name": "MSB - atrium - Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.2586351101519746,
+        "pitch": -0.17089326483875666,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0331843252051014,
+          "pitch": 0.22953035996141935,
+          "rotation": 0,
+          "target": "4-statistics-student-common-room"
+        },
+        {
+          "yaw": -1.8499930458894092,
+          "pitch": 0.12649421226925206,
+          "rotation": 0,
+          "target": "5-seminar-and-teaching-room-001"
+        },
+        {
+          "yaw": -1.3269633525862528,
+          "pitch": 0.11626615776010141,
+          "rotation": 0,
+          "target": "6-seminar-and-teaching-room-002"
+        },
+        {
+          "yaw": 0.0058152038962688835,
+          "pitch": 0.09426254757521768,
+          "rotation": 0,
+          "target": "7-msb---atrium---lifts"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-statistics-student-common-room",
+      "name": "Statistics Student Common Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.0236378906279793,
+        "pitch": 0.07638900200518073,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9003798918597958,
+          "pitch": 0.029366780715339047,
+          "rotation": 0,
+          "target": "3-msb---atrium---entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-seminar-and-teaching-room-001",
+      "name": "Seminar and Teaching Room 001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.0900465253906368,
+        "pitch": 0.053528284306811,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4097185792758449,
+          "pitch": 0.033955078765485425,
+          "rotation": 0,
+          "target": "3-msb---atrium---entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-seminar-and-teaching-room-002",
+      "name": "Seminar and Teaching Room 002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.1936596722376365,
+        "pitch": 0.0996675792827233,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3063163358197603,
+          "pitch": 0.039455959088110504,
+          "rotation": 0,
+          "target": "3-msb---atrium---entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-msb---atrium---lifts",
+      "name": "MSB - atrium - lifts",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.6724969790555466,
+        "pitch": 0.050550295014559765,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.056337131277399,
+          "pitch": 0.043739456642859054,
+          "rotation": 0,
+          "target": "10-msb---atrium---exit"
+        },
+        {
+          "yaw": 1.8005213910384894,
+          "pitch": 0.08373531541156254,
+          "rotation": 0,
+          "target": "9-first-floor---study-space-and-staff-offices"
+        },
+        {
+          "yaw": 0.12091873771997008,
+          "pitch": 0.058793457613171896,
+          "rotation": 0,
+          "target": "3-msb---atrium---entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-first-floor---study-space",
+      "name": "First floor - Study Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.3478284038824668,
+        "pitch": 0.14929055998464236,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.433513227316599,
+          "pitch": 0.18182790864173626,
+          "rotation": 0,
+          "target": "7-msb---atrium---lifts"
+        },
+        {
+          "yaw": 0.8817139701424352,
+          "pitch": 0.03319834129933241,
+          "rotation": 0,
+          "target": "9-first-floor---study-space-and-staff-offices"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-first-floor---study-space-and-staff-offices",
+      "name": "First floor - Study Space and Staff Offices",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.6206069853874148,
+        "pitch": -0.03701987470592272,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8061288876909476,
+          "pitch": 0.047854629688739436,
+          "rotation": 0,
+          "target": "8-first-floor---study-space"
+        },
+        {
+          "yaw": 2.438572494550871,
+          "pitch": 0.2768562207671117,
+          "rotation": 0,
+          "target": "7-msb---atrium---lifts"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-msb---atrium---exit",
+      "name": "MSB - atrium - Exit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.233512045946954,
+        "pitch": 0.17331999263965692,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2230787859086742,
+          "pitch": 0.08180475778555518,
+          "rotation": 0,
+          "target": "11-seminar-and-teaching-room-3"
+        },
+        {
+          "yaw": 0.1254587539832066,
+          "pitch": 0.0731707503350183,
+          "rotation": 0,
+          "target": "7-msb---atrium---lifts"
+        },
+        {
+          "yaw": -2.608860550850938,
+          "pitch": 0.150526801003803,
+          "rotation": 0,
+          "target": "12-zeeman-building---warwick-mathematics-institute"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-seminar-and-teaching-room-3",
+      "name": "Seminar and teaching room 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.2166635799390058,
+        "pitch": 0.1687495541100663,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4090137651448078,
+          "pitch": -0.042107859590368335,
+          "rotation": 0,
+          "target": "10-msb---atrium---exit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-zeeman-building---warwick-mathematics-institute",
+      "name": "Zeeman Building - Warwick Mathematics Institute",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.13485303480630328,
+        "pitch": 0.007310663218138558,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.47753115688055203,
+          "pitch": 0.024929356072004083,
+          "rotation": 0,
+          "target": "13-zeeman-building---the-street"
+        },
+        {
+          "yaw": -2.9210333765157923,
+          "pitch": 0.11564845319053774,
+          "rotation": 0,
+          "target": "0-the-courtyard"
+        },
+        {
+          "yaw": -1.0858916225809665,
+          "pitch": 0.06802107584140593,
+          "rotation": 0,
+          "target": "10-msb---atrium---exit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-zeeman-building---the-street",
+      "name": "Zeeman Building - The Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.6897531661492025,
+        "pitch": -0.11441209211928438,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6766332643444635,
+          "pitch": 0.05808089881679379,
+          "rotation": 0,
+          "target": "14-large-lecture-theatre---ms01"
+        },
+        {
+          "yaw": 1.9687299415608948,
+          "pitch": 0.02237547955158803,
+          "rotation": 0,
+          "target": "12-zeeman-building---warwick-mathematics-institute"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-large-lecture-theatre---ms01",
+      "name": "Large Lecture Theatre - MS.01",
       "levels": [
         {
           "tileSize": 256,
@@ -66,11 +656,56 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.4131637562610946,
+          "pitch": 0.15772867817628544,
+          "rotation": 0,
+          "target": "13-zeeman-building---the-street"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-campus-centre---the-piazza",
+      "name": "Campus Centre - The Piazza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -3.005231828709139,
+        "pitch": -0.056034630052533174,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.487705000890898,
+          "pitch": 0.032578856416268565,
+          "rotation": 0,
+          "target": "1-university-road"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "MSB_Virtual Tour",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
